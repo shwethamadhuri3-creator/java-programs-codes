a331803f7082604d599e593de740f8021dd67044
Madhuri <shwethamadhuri3@gmail.com>
2025-11-27 19:30:39 +0530

    Add files via upload

---
+++ VariableDemo.java
@@ -0,0 +1,14 @@
+
+class VariableDemo {
+    public static void main(String[] args) {
+        int age = 25;               
+        double salary = 55000.75;   
+        char grade = 'A';           
+        boolean isStudent = false;  
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
